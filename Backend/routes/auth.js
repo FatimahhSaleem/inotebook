87092906e62cea5c4397c8b5fae8af4255e86223
Fatima <you@example.com>
2024-02-21 15:48:45 +0500

    created jwt token and bcrypt the password using hash

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,12 @@ const express=require('express');
 const User=require('../modules/User')
 const router=express.Router();
 const { query, validationResult, body } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+//jwt token 
+const JWT_SECRET='dfghjertydcfvghbjrtyh'
 
 
 //Create a user using :POST "/api/auth/createuser" No login required
@@ -25,16 +31,34 @@ if (existingUser) {
   return res.status(400).json({ error: 'Email already exists' });
 }
 
+
+
+
 try {
+
+    //bcrypt the password using hash
+    const salt=await bcrypt.genSalt(10);
+    const secPass=await bcrypt.hash(req.body.password,salt) ;
+
   // Create a new user
+
   let user = await User.create({
     name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
+    email: req.body.email, 
+    password: secPass
   });
+  //return token to user for authentication
+
+  const data={
+    user:{
+        id:user.id
+    }
+  }
+  const authtoken=jwt.sign(data,JWT_SECRET)
 
   // Respond with the created user
-  res.json(user);
+  res.json({authtoken});
+
 } catch (error) {
   if (error.code === 11000) {
     // Duplicate key error
