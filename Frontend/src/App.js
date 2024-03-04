6c07b7bc2c287d1f46542f34f097cdb346e8aa97
Fatima <you@example.com>
2024-03-04 18:39:12 +0500

    added from in home component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,29 +1,27 @@
-import './App.css';
-import { BrowserRouter as Router,
-Routes,Route} from "react-router-dom";
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import NoteState from './context/notes/NoteState';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
-   <>
-   <NoteState>
-   < Router>
-    <Navbar/>
-    <Routes>
-      <Route exact path='/' element={<Home/>}></Route>
-    </Routes>
-    <Routes>
-      <Route exact path='/about' element={<About/>}></Route>
-    </Routes>
-
-
-
-   </Router>
-   </NoteState>
-   </>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />}></Route>
+            </Routes>
+            <Routes>
+              <Route exact path="/about" element={<About />}></Route>
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
